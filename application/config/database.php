17a85a3a36d1d70d24399400dede3feb9646d69d
qixingyue <istrone@hello-world-thinkpad-e40.(none)>
2013-04-23 20:19:36 +0800

    edit database

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'qixingyue';
+$db['default']['password'] = '123456abc';
+$db['default']['database'] = 'qixingyue_app68';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
